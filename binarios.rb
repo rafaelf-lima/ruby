c0a5839dbc2ea5918dd0310077e8bfbeefa1521a
Rafael Lima <105614039+rafaelf-lima@users.noreply.github.com>
2023-04-15 19:34:09 -0300

    Update binarios.rb

--- binarios.rb
+++ binarios.rb
@@ -20,19 +20,19 @@ def operacoes(binario_string)
   decimal = binario_string.to_i(2)
   puts "\nO valor da palavra em decimal é #{decimal}"
   if decimal < 16
-    puts "\nA operação escolhida foi complemento de 1!"
+    puts "\nA operação escolhida foi complemento de 1"
     valor = complemento1(binario_string)
   elsif decimal < 32
-    puts "\nA operação escolhida foi complemento de 2!"
+    puts "\nA operação escolhida foi complemento de 2"
     valor = complemento2(binario_string)
   elsif decimal < 48
-    puts "\nA operação escolhida foi adição!"
+    puts "\nA operação escolhida foi adição"
     valor = adicao(binario_string)
   else
-    puts "\nA operação escolhida foi subtração!"
+    puts "\nA operação escolhida foi subtração"
     valor = subtracao(binario_string)
   end
-  puts "\nO resultado da operação escolhida é #{valor}!"
+  puts "O resultado da operação escolhida é #{valor}!"
   
 end  
 
@@ -76,18 +76,19 @@ def subtracao(binario_string)
   segunda_parte = binario_string[3,3]
   inteiro_um = primeira_parte.to_i(2)
   inteiro_dois = segunda_parte.to_i(2)
-  if inteiro_um >= inteiro_dois
+  if inteiro_um > inteiro_dois
     subtracao = inteiro_um - inteiro_dois
     res = subtracao.to_s(2)
   else
-    res = inteiro_dois - inteiro_um
+    subtracao = inteiro_dois - inteiro_um
+    res = subtracao.to_s(2)
     case MODO
       when 0
-        res.to_s(2)[0] = '1'
+        res[0] = '1'
       when 1
-        complemento1(res.to_s(2))
+        complemento1(res)
       when 2
-        complemento2(res.to_s(2))
+        complemento2(res)
     end
   end
   return res.rjust(6, '0')
